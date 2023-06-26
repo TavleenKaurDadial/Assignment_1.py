5480ed37cc7648a9c5a2429fa3316817e4a93220
TavleenKaurDadial <137488988+TavleenKaurDadial@users.noreply.github.com>
2023-06-26 18:16:22 +0530

    Add files via upload

---
+++ Assignment1.py
@@ -0,0 +1,5 @@
+marks1=int(input("Enter marks1"))
+marks2=int(input("Enter marks2"))
+marks3=int(input("Enter marks3"))
+avg=(marks1+marks2+marks3)/3
+print("Average of ",marks1 ,",", marks2 ,"&", marks3 ,"is",avg)
